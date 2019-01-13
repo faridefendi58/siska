816c0dafd901819d955ba303bda0b04dfeda7a82
Farid Efendi <miftahfaridse@gmail.com>
2019-01-13 16:09:39 +0700

    Payment cash method

--- app/src/main/java/com/slightsite/siskapos/ui/transaction/CheckoutActivity.java
+++ app/src/main/java/com/slightsite/siskapos/ui/transaction/CheckoutActivity.java
@@ -8,9 +8,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.slightsite.siskapos.R;
@@ -28,6 +33,10 @@ public class CheckoutActivity extends AppCompatActivity {
     private AdapterListCartCheckout mAdapter;
     private Register register;
     private View parent_view;
+    private EditText colected_cash;
+    private TextView cash_change;
+    private TextView sub_total;
+    private TextView total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +57,52 @@ public class CheckoutActivity extends AppCompatActivity {
 
         parent_view = findViewById(R.id.parent_view);
 
+        double total_cart = 0;
         if(register.hasSale()){
             if (register.getTotal() > 0) {
                 showList(register.getCurrentSale().getAllLineItem());
+                total_cart = register.getTotal();
             }
         } else{
             showList(new ArrayList<LineItem>());
         }
+
+        cash_change = (TextView) findViewById(R.id.cash_change);
+        sub_total = (TextView) findViewById(R.id.sub_total);
+        sub_total.setText(CurrencyController.getInstance().moneyFormat(total_cart));
+        total = (TextView) findViewById(R.id.total);
+        total.setText(CurrencyController.getInstance().moneyFormat(total_cart));
+
+        colected_cash = (EditText) findViewById(R.id.colected_cash);
+        try {
+            int i_total_cart = (int) total_cart;
+            colected_cash.setHint(i_total_cart);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        colected_cash.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+                double tot = register.getTotal();
+                double col_tot = 0;
+                try {
+                    col_tot = Double.parseDouble(s.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                double change_val = 0;
+                if (col_tot >= tot) {
+                    change_val = col_tot - tot;
+                }
+                Log.e("CheckoutActivity", "change_val : "+ change_val);
+                cash_change.setText(CurrencyController.getInstance().moneyFormat(change_val));
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
     }
 
     private void initToolbar() {
